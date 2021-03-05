9fdf6fcf59696e252c44844d7364229ac2ee3b9d
patricksingleton86 <patricksingleton95@gmail.com>
2021-03-05 13:36:44 +0000

    Added checkout functions and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -5,7 +5,7 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = {'full_name', 'email', 'phone_number'
+        fields = {'full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country',
                   'county'}
@@ -30,7 +30,7 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[fields].required:
+            if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
             else:
                 placeholder = placeholders[field]
